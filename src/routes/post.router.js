afde0e6def0368d314db318d79f728863624dd0f
arieltoniatto <ariel.toniatto@gmail.com>
2022-09-27 17:32:10 -0300

    isolando req16 por estar quebrando a aplicação e impossibilitando rodar os req17 e 18

--- src/routes/post.router.js
+++ src/routes/post.router.js
@@ -1,16 +1,18 @@
 const express = require('express');
 const validateToken = require('../middleware/validateToken');
 const {
-    createPost, getPost, getPostById, updatePost, deletePost, querySearch,
+    createPost, getPost, getPostById, updatePost,
+    // deletePost,
+    querySearch,
 } = require('../controller/post.controller');
 
 const routers = express.Router();
 
+routers.get('/search', validateToken, querySearch);
 routers.post('/', validateToken, createPost);
 routers.get('/', validateToken, getPost);
-routers.get('/search', validateToken, querySearch);
-routers.delete('/:id', validateToken, deletePost);
 routers.get('/:id', validateToken, getPostById);
 routers.put('/:id', validateToken, updatePost);
+// routers.delete('/:id', validateToken, deletePost);
 
 module.exports = routers;
